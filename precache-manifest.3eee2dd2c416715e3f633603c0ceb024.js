9d57cdf609a7626bb3b5313a90fe50ab037ed93e
Brian Loveless <Brian.loveless1@yahoo.com>
2020-10-18 11:00:54 -0400

    Updates

---
+++ precache-manifest.3eee2dd2c416715e3f633603c0ceb024.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb30b37e6b49660bc0b2024d4c516f5c",
+    "url": "/LegoRules/index.html"
+  },
+  {
+    "revision": "a1a4715e2a9ea09983b9",
+    "url": "/LegoRules/static/css/main.4626fa3c.chunk.css"
+  },
+  {
+    "revision": "2f2d6d8f064208b328c5",
+    "url": "/LegoRules/static/js/2.80deb981.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/LegoRules/static/js/2.80deb981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a4715e2a9ea09983b9",
+    "url": "/LegoRules/static/js/main.550ef2f8.chunk.js"
+  },
+  {
+    "revision": "48252d58f3708d568faf",
+    "url": "/LegoRules/static/js/runtime-main.bb7b4d7f.js"
+  },
+  {
+    "revision": "4a5406c364bfca2dfea817d45297f296",
+    "url": "/LegoRules/static/media/21044.4a5406c3.jpg"
+  },
+  {
+    "revision": "f0f50f841cfd9bff4ccb9e00986cfecd",
+    "url": "/LegoRules/static/media/40407.f0f50f84.jpeg"
+  },
+  {
+    "revision": "a51d476ac358b96c5f18f55f0ed146e0",
+    "url": "/LegoRules/static/media/75236.a51d476a.jpeg"
+  },
+  {
+    "revision": "eacbb54330828bada01dbd11d1a4ac09",
+    "url": "/LegoRules/static/media/75257.eacbb543.jpg"
+  },
+  {
+    "revision": "486e0440deabc28ea4925e76acd5e62d",
+    "url": "/LegoRules/static/media/76159.486e0440.jpeg"
+  },
+  {
+    "revision": "0028367c672498a6639030fa0b15d83c",
+    "url": "/LegoRules/static/media/7956.0028367c.jpg"
+  }
+]);
